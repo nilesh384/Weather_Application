5d0fcea37f8e050c68808896336e13f16ea91ce2
Nilesh Bera <nileshbera258@gmail.com>
2024-09-01 16:49:34 +0530

    updated

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const temperature = document.querySelector(".temp");
 const description = document.querySelector(".description");
 const humidity = document.querySelector(".humid-degree");
 const windSpeed = document.querySelector(".windSpeed");
+const temp_sup = document.querySelector(".temp-sup");
+const humidity_icon = document.querySelector(".fa-droplet");
+const wind_icon = document.querySelector(".fa-wind");
 
 const API_KEY = "b167300729msh9af3142aab02a77p1dd36ejsna1722ffef940";
 const API_HOST = "open-weather13.p.rapidapi.com";
@@ -34,8 +37,8 @@ async function fetchWeather(city) {
 
         temperature.innerHTML = `${Math.floor((main.temp-32)*.56)}`;
         description.innerHTML = `${weather.description}`;
-        humidity.innerHTML = `Humidity: ${main.humidity}%`;
-        windSpeed.innerHTML = `Wind Speed: ${wind.speed} m/s`;
+        humidity.innerHTML = `${main.humidity}%`;
+        windSpeed.innerHTML = `${wind.speed} m/s`;
 
         // Set weather icon based on the icon code returned by the API
         weatherIcon.src = `http://openweathermap.org/img/wn/${weather.icon}.png`;
@@ -47,7 +50,11 @@ async function fetchWeather(city) {
         description.innerHTML = 'Unable to fetch weather data';
         humidity.innerHTML = '';
         windSpeed.innerHTML = '';
-        weatherIcon.src = '';
+        weatherIcon.src = ''
+        temp_sup.innerHTML = ''
+        humidity_icon.classList.add('hidden');
+        wind_icon.classList.add('hidden');
+
     }
 }
 

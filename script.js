ad99caf4c4ff83d57320c8801bd55edc5fb93d49
Nilesh Bera <nileshbera258@gmail.com>
2024-09-01 12:30:15 +0530

    html, css added -- working on java

---
+++ script.js
@@ -0,0 +1,7 @@
+const search = document.querySelector(".search-bar");
+const weatherIcon = document.querySelector(".weather-icon");
+
+const temperature = document.querySelector(".temp");
+const description = document.querySelector(".description");
+const humidity = document.querySelector(".humid-degree");
+const windSpeed = document.querySelector(".windSpeed");
